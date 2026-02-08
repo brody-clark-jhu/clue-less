04ca10a9b6e2dbb84cdc9744a40b87979bb6e436
brody-clark-jhu <bclark49@jh.edu>
2026-02-08 11:03:25 -0500

    init

---
+++ UI.ts
@@ -0,0 +1,24 @@
+const canvas = document.getElementById("game-board")!;
+
+interface BoardNode {
+  id: string;
+  x: number;
+  y: number;
+  radius: number; // or width/height
+  neighbors: string[];
+}
+
+function findNodeAt(x:number, y:number){
+ return BoardNode;
+}
+
+canvas.addEventListener("click", (e) => {
+  const rect = canvas.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  const node = findNodeAt(x, y);
+  if (!node) return;
+
+  sendMoveRequest(node.id);
+});
