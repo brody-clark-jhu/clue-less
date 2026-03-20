6b367dad19437e1393fb5c00776030e567321213
maksimowiczchris-cmd <maksimowiczchris@gmail.com>
2026-03-20 10:38:39 -0400

    Implmented Home page with transition to Lobby page, where the message request can be seen

--- client/src/view.ts
+++ client/src/view.ts
@@ -8,14 +8,35 @@ export function onRequestButtonClick(cb: RequestButtonCallback) {
 export class View {
   displayTxt: HTMLElement;
   button: HTMLElement;
+
   constructor() {
     this.displayTxt = document.getElementById("display-message")!;
     this.button = document.getElementById("request")!;
+
     this.button.onclick = () => {
       if (requestButtonCallback) {
         requestButtonCallback();
       }
     };
+
+    //Changes the front page to the lobby page
+    const joinLobbyBtn = document.getElementById("btn-join-lobby");
+    if (joinLobbyBtn) {
+      joinLobbyBtn.addEventListener("click", () => {
+        this.showScreen("lobby-screen");
+      });
+    }
+  }
+
+ //Switch screens using the ID
+  public showScreen(screenId: string): void {
+    document.querySelectorAll(".screen").forEach((s) => {
+      s.classList.remove("active");
+    });
+    const target = document.getElementById(screenId);
+    if (target) {
+      target.classList.add("active");
+    }
   }
 
   public SetDisplayMessage(msg: string) {
